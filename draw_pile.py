bfdee948f47235080e4b9cb0c68e696fef13cf19
sarahsvahn <sarah.svahn@gmail.com>
2025-04-02 15:48:54 -0400

    UPDATE draw_pile, not tested

--- draw_pile.py
+++ draw_pile.py
@@ -1,33 +1,42 @@
 import random
 
 class DrawPile(): 
-
     def __init__(self, cards):
-        self.cards = cards
-        self.idx = len(cards) - 1
+        self.faceDown = cards # top of deck (idx len - 1) is back
+        self.faceUp = []      # first card is front (idx 0)
 
     def getCard(self):
-        return self.cards[self.idx]
+        return self.faceUp[0]
 
     def drawThree(self):
-        # [1, 2, 3, 4, 5]
-        # would want to return [3, 4, 5]
-        # idx is 5
-        # [1, 2]
-        # start_idx -1 -> 0
-        # check if self.idx 
-        if self.idx == 0:
-            # start over 
-        start_idx = self.idx - 2
-        if start_idx < 0:
-            start_idx = 0
-        return self.cards[start_idx:self.idx]
+        if len(self.faceDown) == 0:
+            self.faceDown = self.faceUp
+            self.faceUp = []
+        
+        num_flipped = 0
+        while len(self.faceDown) != 0 and num_flipped < 3: 
+            self.faceUp.append(self.faceDown.pop())
+            num_flipped += 1
+        
+        return self.getTopThree()
+        
+    def getTopThree(self):
+        num_checked = 0
+        top_three = []
+        while len(self.faceDown) != 0 and num_checked < 3: 
+            top_three.append(self.faceUp[num_checked])
+        
+        return top_three
+    
 
     def takeCard(self): 
+        ''' 
+        Returns: Tuple of card removed and list of new top 3 cards
+        Note:    Parent module verifies move before calling this      
+        '''
         card = self.cards[self.idx]
         self.cards.remove(card)
-        return card
-
+        return (card, self.getTopThree())
 
     def shuffle(self): 
         random.shuffle(self.cards)
