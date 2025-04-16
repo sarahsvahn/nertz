fd24fb23bc53b66deddd51297d4c5a1a3cfccb3c
CliodhnaR <cliodhna.reidy@tufts.edu>
2025-04-16 18:51:33 -0400

    Working multiple rounds

--- client.py
+++ client.py
@@ -7,187 +7,180 @@ import curses
 from windows import Windows
 import sys
 
+# TODO make name a member variable of Hand, with a getter and a setter func - done, not tested
+
 # TODO starting the next game doesn't work 
 # TODO shuffle
 # TODO alert player when the draw deck is turned over 
 # TODO indicate which card you are allowed to take from top3 and wps visually
-# TODO make name a member variable of Hand, with a getter and a setter func
 # TODO say who got nertz
 # TODO put everything in client.py in a class
 
-sio = socketio.Client()
 
-cp_move_done = threading.Event()
+class Client():
+    def __init__(self, stdscr):
+        self.sio = socketio.Client()
+        self.cp_move_done = threading.Event()
+        self.server_url = "http://localhost:5000"
+        self.hand = Hand()
+        self.windows = Windows(stdscr)
+        self.event = threading.Event()
+        self.query = None
+        self.thread = None
 
-server_url = "http://localhost:5000"
+        self.setup_handlers()
+    
+    @classmethod
+    def validate_card(cls, card_name):
+        print(card_name)
+        card_letter = card_name[-1]
+        if card_letter.isalpha():
+            if card_letter.upper() not in ["D", "H", "C", "S"]:
+                return Status.INVALID_CARD
+        else:
+            return Status.INVALID_CARD
+        if card_name[:-1].isnumeric():
+            if int(card_name[:-1]) > 13 or int(card_name[:-1]) <= 0:
+                return Status.INVALID_CARD
+        return Status.SUCCESS     
 
-hand = Hand()
-name = "" 
+    def establish_player(self):
+        self.windows.input_write("Welcome to Nertz! Enter your name: ")
+        name = self.windows.input_read()
+        self.hand.set_name(name)
+        self.windows.input_refresh()
+        self.sio.emit("player_join", {"name": name})
+        self.sio.wait()
+    
+    def print_scores(self, scores):
+        self.windows.print_scores(scores)
 
-windows = None
+    # thread function
+    def input_thread(self): 
+        self.query = self.windows.input_read().lower()
+        self.event.set()
+        self.thread = None
 
-event = threading.Event() # TODO not global
-query = None # TODO not global 
-thread = None # TODO not global 
+    def setup_handlers(self):
+        @self.sio.on("get_scores")
+        def send_score(data):
+            self.sio.emit("my_score", {"score": self.hand.get_score(), "name": self.hand.get_name()})
+
+        @self.sio.on("game_joined")
+        def handle_game_joined(data):
+            self.sio.wait()
+
+        @self.sio.on("cp_move_result")
+        def cp_move_result(data):
+            if Status[data.get("status")] == Status.SUCCESS:
+                remove_location = data.get("origin")
+                self.hand.remove_from_origin(Origin[remove_location])
+            else: 
+                self.windows.error_write(1, 1, "Move failed. RIPPPPP that sucks.")
+                self.windows.error_refresh()
+
+            self.cp_move_done.set()
+
+            # self.thread = self.cp_move_done.set()
+
+        @self.sio.on("reset")
+        def reset(data): 
+            self.event.set()
+
+            scores = data.get("scores")
+            self.print_scores(scores)
+            self.hand.reset_hand()
+
+            self.windows.input_refresh()
+            self.windows.hand_refresh()
+            self.windows.input_write("Enter any key to start the next round: ")
+            self.event.wait()
+            self.event.clear()
+            self.sio.emit("test", {"parameter": "entered key"})
+            self.sio.emit("player_rejoin")
+            
+        @self.sio.on("game_over")
+        def game_over(data): 
+            scores = data.get("scores")
+            Client.print_scores(scores)
+            winner = min(scores, key=scores.get)
+            self.windows.community_write(f"{winner} is the winner!", len(scores) + 1, 1)
+            self.windows.community_refresh()
+
+        @self.sio.on("cs_updated")
+        def update_cs(data):
+            board = data.get("board")
+            self.windows.print_cs(board)
+
+        @self.sio.on("start_game")
+        def query_loop(): 
+            curses.echo()
+
+            self.windows.community_refresh()
+            self.windows.input_refresh()
+            self.windows.print_board(self.hand, self.hand.get_name())
+
+            self.windows.input_write("> ")
+            self.query = self.windows.input_read().lower()
+            
+            while self.query != None: 
+                self.sio.emit("test", {"parameter": "Starting loop"})
+                self.sio.emit("test", {"parameter": f"Query: {self.query}"})
+                self.windows.error_refresh()
+                if len(self.query) == 0: 
+                    self.windows.error_write("Usage: m <card> <pile> | m <ace> cp | d | s | nertz")
+                else: 
+                    self.query = self.query.split()
+                    if self.query[0] == 'm' and len(self.query) == 3:
+                        if Client.validate_card(self.query[1]) == Status.INVALID_CARD:
+                            self.windows.error_write("Invalid move")
+                        else:
+                            if "cp" in self.query[2]:
+                                origin = self.hand.find_og_location(Card.card_with_name(self.query[1]), "CP")
+                                if origin != Origin.NOT_FOUND:
+                                    self.cp_move_done.clear()
+                                    self.sio.emit("cp_move", {'card': self.query[1], 'pile': self.query[2], "name": self.hand.get_name(), "origin": origin.name})
+                                    self.cp_move_done.wait()
+                                else:
+                                    self.windows.error_write("Invalid move")
+                            elif "wp" in qself.self.uery[2]: 
+                                result = self.hand.move_to_wp(self.query[1], self.query[2])
+                                if result == Status.INVALID_MOVE: 
+                                    self.windows.error_write("Invalid move")
+                            else: 
+                                self.windows.error_write("Usage: m <card> <pile> | m <ace> cp | d | s | nertz")
+                    elif self.query == ['d']: 
+                        self.hand.draw()
+                    elif self.query == ['s']:
+                        self.hand.shuffle()
+                        # TODO 
+                    elif self.query == ['nertz']:
+                        self.sio.emit("test", {"parameter": "You think you have nertz?"})
+                        if self.hand.has_nertz():
+                            self.sio.emit("has_nertz")
+                        else: 
+                            self.windows.error_write("Your nertz pile is not empty. Keep playing.")
+                    else: 
+                        self.windows.error_write("Usage: m <card> <pile> | m <ace> cp | d | s | nertz")
+
+                self.windows.print_board(self.hand, self.hand.get_name())
+                self.windows.input_write("> ")
+                self.query = None
+                self.thread = threading.Thread(target=self.input_thread, args=()).start()
+                self.event.wait()
+                self.event.clear()
+
+            self.sio.emit("test", {"parameter": "After loop"})
+
+    def connect(self):
+        self.sio.connect(self.server_url)
 
 def main(stdscr):
-    global hand, windows
-    windows = Windows(stdscr)
-    sio.connect(server_url)
-    establish_player()
-
-def establish_player():
-    global name, windows
-    windows.input_write("Welcome to Nertz! Enter your name: ")
-    name = windows.input_read()
-    windows.input_refresh()
-    sio.emit("player_join", {"name": name})
-    sio.wait()
-
-@sio.on("get_scores")
-def send_score(data):
-    global name, hand
-    sio.emit("my_score", {"score": hand.get_score(), "name": name})
-
-@sio.on("game_joined")
-def handle_game_joined(data):
-    sio.wait()
-
-@sio.on("cp_move_result")
-def cp_move_result(data):
-    global hand
-    if Status[data.get("status")] == Status.SUCCESS:
-        remove_location = data.get("origin")
-        hand.remove_from_origin(Origin[remove_location])
-    else: 
-        windows.error_write(1, 1, "Move failed. RIPPPPP that sucks.")
-        windows.error_refresh()
-
-    cp_move_done.set()
-
-
-@sio.on("reset")
-def reset(data): 
-    global windows, event
-
-    event.set()
-
-    scores = data.get("scores")
-    print_scores(scores)
-    hand.reset_hand()
-
-    windows.input_refresh()
-    windows.hand_refresh()
-    windows.input_write("Enter any key to start the next round: ")
-    event.wait()
-    # windows.input_read()
-    sio.emit("test", {"parameter": "entered key"})
-    sio.emit("player_rejoin")
-           
-@sio.on("game_over")
-def game_over(data): 
-    scores = data.get("scores")
-    print_scores(scores)
-    winner = min(scores, key=scores.get)
-    windows.community_write(f"{winner} is the winner!", len(scores) + 1, 1)
-    windows.community_refresh()
-
-def print_scores(scores):
-    global windows
-    windows.print_scores(scores)
-
-# thread function
-def input_thread(event): 
-    global query, thread
-    query = windows.input_read().lower()
-    event.set()
-    thread = None
-    # sio.emit("test", {"parameter": ""})
-
-@sio.on("start_game")
-def query_loop(): 
-    global name, hand, windows, query, thread
-    curses.echo()
-
-    windows.community_refresh()
-    windows.input_refresh()
-    windows.print_board(hand, name)
-
-    windows.input_write("> ")
-    query = windows.input_read().lower()
-    
-    while query != None: 
-        sio.emit("test", {"parameter": "Starting loop"})
-        sio.emit("test", {"parameter": f"Query: {query}"})
-        windows.error_refresh()
-        if len(query) == 0: 
-            windows.error_write("Usage: m <card> <pile> | m <ace> cp | d | s | nertz")
-        else: 
-            query = query.split()
-            if query[0] == 'm' and len(query) == 3:
-                if validate_card(query[1]) == Status.INVALID_CARD:
-                    windows.error_write("Invalid move")
-                else:
-                    if "cp" in query[2]:
-                        origin = hand.find_og_location(Card.card_with_name(query[1]), "CP")
-                        if origin != Origin.NOT_FOUND:
-                            cp_move_done.clear()
-                            sio.emit("cp_move", {'card': query[1], 'pile': query[2], "name": name, "origin": origin.name})
-                            cp_move_done.wait()
-                        else:
-                            windows.error_write("Invalid move")
-                    elif "wp" in query[2]: 
-                        result = hand.move_to_wp(query[1], query[2])
-                        if result == Status.INVALID_MOVE: 
-                            windows.error_write("Invalid move")
-                    else: 
-                        windows.error_write("Usage: m <card> <pile> | m <ace> cp | d | s | nertz")
-            elif query == ['d']: 
-                hand.draw()
-            elif query == ['s']:
-                hand.shuffle()
-                # TODO 
-            elif query == ['nertz']:
-                if hand.has_nertz():
-                    sio.emit("has_nertz")
-                else: 
-                    windows.error_write("Your nertz pile is not empty. Keep playing.")
-            else: 
-                windows.error_write("Usage: m <card> <pile> | m <ace> cp | d | s | nertz")
-
-        windows.print_board(hand, name)
-        windows.input_write("> ")
-        query = None
-        thread = threading.Thread(target=input_thread, args=(event,)).start()
-        event.wait()
-        event.clear()
-
-    sio.emit("test", {"parameter": "After loop"})
-    
-
-
-
-def validate_card(card_name):
-    print(card_name)
-    card_letter = card_name[-1]
-    if card_letter.isalpha():
-        if card_letter.upper() not in ["D", "H", "C", "S"]:
-            return Status.INVALID_CARD
-    else:
-        return Status.INVALID_CARD
-    if card_name[:-1].isnumeric():
-        if int(card_name[:-1]) > 13 or int(card_name[:-1]) <= 0:
-            return Status.INVALID_CARD
-    return Status.SUCCESS       
-
-@sio.on("cs_updated")
-def update_cs(data):
-    global windows
-    board = data.get("board")
-    windows.print_cs(board)
+    client = Client(stdscr)
+    client.connect()
+    client.establish_player()
 
 curses.wrapper(main)
-
 
 if __name__ == "__main__":
     main()
--- hand.py
+++ hand.py
@@ -17,6 +17,13 @@ import random
 class Hand():
     def __init__(self): 
         self.reset_hand()
+        self.name = ""
+
+    def set_name(self, name): 
+        self.name = name
+
+    def get_name(self):
+        return self.name
 
     def reset_hand(self):
         ''' 
