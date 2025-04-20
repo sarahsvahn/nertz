fee43076d64e60d03bf5f48d190b4a45d68520cd
CliodhnaR <cliodhna.reidy@tufts.edu>
2025-04-20 16:27:46 -0400

    mini deck

--- hand.py
+++ hand.py
@@ -36,7 +36,7 @@ class Hand():
         self.working_piles = [WorkingPile(deck[0]), WorkingPile(deck[1]),
                               WorkingPile(deck[2]), WorkingPile(deck[3])]
         # self.nertz_pile = deck[4:17] #TODO uncomment this line
-        self.nertz_pile = [Card("D", "1")] #TODO remove this line
+        self.nertz_pile = [Card("D", "1"), Card("D", "2")] #TODO remove this line
         self.draw_pile = DrawPile(deck[17:])
         self.score = 100 #-26
     
